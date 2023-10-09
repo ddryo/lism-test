d3f068fb13d1f3bbc0a969da570d502e3062f981
ddryo <dd.bocsfcryo@gmail.com>
2023-10-09 15:21:26 +0900

    add text.mdx

--- packages/lism-core/src/components/Text/Text.jsx
+++ packages/lism-core/src/components/Text/Text.jsx
@@ -1,33 +1,8 @@
 import React from 'react';
-import { getLismProps } from '../../lib';
-// import classnames from 'classnames';
-
-// R ?
-// const fzSets = ["6L", "5L", "4L", "3L", "2L", "L", "M", "S", "2S"];
-// const lhSets = ["1", "1.2", "1.4", "1.5", "1.6", "1.8", "2"];
-// const taSets = ['center', 'left', 'right', 'c', 'r', 'l'];
-
-// const taProp = ["center", "left", "right"];
+import { Core } from '../Core';
 
 // 省略可能にするかどうかは、要検討
-export default function Text({ children, as, tag, ...props }) {
-	// 省略しててもOK
-
-	const { className, style, attrs } = getLismProps(props);
-
-	// ta から1文字目を取得
-	// if (ta && taSets.includes(ta)) {
-	// 	classNames.push(`-ta:${ta[0]}`);
-	// }
-
-	const blockProps = {
-		className,
-		style,
-		...attrs,
-	};
-
-	// console.log("attrs", attrs);
-	const Tag = as || tag || 'p';
-	// useTextProps?
-	return <Tag {...blockProps}>{children}</Tag>;
+export default function Text({ tag = 'p', ...props }) {
+	// 一部のプロパティはTextでのみ受け取れるようにするか？
+	return <Core tag={tag} {...props} />;
 }
